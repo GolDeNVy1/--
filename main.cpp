e6662d8bb0102a94644ed1ef3811eddb1b5be8e4
Денис Зуй <zujdenis7@gmail.com>
2025-02-01 06:43:10 +0200

    better

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+//#include <Windows.h>
 
 int main() {
-    setlocale(LC_ALL, "rus");
+
+    //SetConsoleOutputCP(1251);
+    //SetConsoleCP(1251); не працює у мене бо я на лінуксі, і у мене без цих махінацій усе працює
+
+
     std::cout << "\tH\bel\flo, \\Wo\'r\"ld!\n" << std::endl;
     std::cout << "My n\ram\?e is D\\enis" << std::endl;
-    std::cout << "Я вчу багато мов програмування" << std::endl;
+    std::cout << "Я вчу різні мови програмування!" << std::endl;
+
+
     return 0;
 }
