362b2f71a11e2876ecaad0c569ef1ec619ba1765
Денис Зуй <zujdenis7@gmail.com>
2025-02-01 06:33:04 +0200

    russian is here

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
 int main() {
+    setlocale(LC_ALL, "rus");
     std::cout << "\tH\bel\flo, \\Wo\'r\"ld!\n" << std::endl;
     std::cout << "My n\ram\?e is D\\enis" << std::endl;
+    std::cout << "Я вчу багато мов програмування" << std::endl;
     return 0;
 }
